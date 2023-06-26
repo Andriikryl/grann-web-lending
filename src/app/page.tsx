3bda32968aad1809dc4d07a9518c31b67e6c1cb5
Andriikryl <andreyk557@gmail.com>
2023-06-26 15:26:20 +0300

    add container component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Container } from "./components/contaniner/Container";
 
 export default function Home() {
   return (
     <div>
-      <p>asd</p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque eaque
-      assumenda voluptas, est temporibus vitae cum, ab omnis voluptate odit
-      excepturi magni, reprehenderit labore animi rem molestiae nostrum fugit!
-      Cumque.
+      <Container>
+        <h2>sad</h2>
+      </Container>
     </div>
   );
 }
