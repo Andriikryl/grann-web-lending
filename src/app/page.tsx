9e07a5d93264465be616a13f2fb4244a1f2c3da9
Andriikryl <andreyk557@gmail.com>
2023-06-30 22:12:31 +0300

    add responsive to the socioal section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
       <Info />
       <Delivery />
       <About />
-      {/* <Questions /> */}
-      {/* <Social /> */}
+      <Questions />
+      <Social />
     </>
   );
 }
