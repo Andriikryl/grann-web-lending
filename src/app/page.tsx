d1669de0c734294138651e009db13eb9fe08de4e
Andriikryl <andreyk557@gmail.com>
2023-06-30 21:57:18 +0300

    add responsive to the about section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <Best />
       <Info />
       <Delivery />
-      {/* <About /> */}
+      <About />
       {/* <Questions /> */}
       {/* <Social /> */}
     </>
