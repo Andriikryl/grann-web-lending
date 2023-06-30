3eb051fc4afe9ea392e01d496db2dc28d25ee772
Andriikryl <andreyk557@gmail.com>
2023-06-30 16:32:43 +0300

    add addaptive to the hero section component

--- src/app/components/sections/hero/SubTitel.tsx
+++ src/app/components/sections/hero/SubTitel.tsx
@@ -3,13 +3,13 @@ import styles from "./style.module.css";
 
 export default function SubTitel() {
   return (
-    <h1 className={styles.test}>
+    <span className={styles.test}>
       авторські
       <span className={styles.slider}>
         <span className={styles.slider__word}>десерти</span>
         <span className={styles.slider__word}>Торти</span>
         <span className={styles.slider__word}>Шу</span>
       </span>
-    </h1>
+    </span>
   );
 }
